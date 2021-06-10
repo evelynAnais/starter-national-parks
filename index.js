30a82b2fdd325926f0ef140febd3b52b6bec05f7
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-06-10 07:51:16 -0600

    practice

--- index.js
+++ index.js
@@ -1,3 +1,121 @@
+/*--------------- Event listeners -----------------*/ 
+
+/*HAVE QUESTIONS*/
+// // Declare handler and support functions here
+
+// // Function for sorting by name
+// const sortByName = ...
+
+// // Function for sorting by rating
+// const sortByRating = ...
+
+// // Function for handling the `nameSorter` click
+// const nameSorterClickHandler = ...
+
+// // Function to handle the `ratingSorter` click
+// const ratingSorterClickHandler = ...
+
+
+// // The code that runs once the DOM is loaded
+// const main = () => {
+//   // Select the `nameSorter` link
+//   const nameSorter = document.querySelector("#name-sorter");
+
+//   // Add an event listener
+//   nameSorter.addEventListener("click", nameSorterClickHandler);
+
+//   // Select the `ratingSorter` link
+//   const ratingSorter = document.querySelector("#rating-sorter");
+
+//   // Add an event listener
+//   ratingSorter.addEventListener("click", ratingSorterClickHandler);
+// }
+
+// // Add event listener for `DOMContentLoaded`
+// window.addEventListener("DOMContentLoaded", main)
+
+
+console.log('Before!');
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('Loaded');
+});
+console.log('After!')
+
+// const firstBtn = document.querySelector("button");
+// firstBtn.addEventListener("click", (event) => {
+//   console.log("You clicked the button", event);
+// });
+
+const allBtns = document.querySelectorAll('.rate-button')
+allBtns.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = '#c8e6c9'
+  })
+})
+
+const nameSorter = document.querySelector('#name-sorter');
+  // nameSorter.addEventListener('click', (event) => {
+  //   event.preventDefault();
+  //   console.log('You clicked hte name sorter');
+  // });
+
+  nameSorter.addEventListener('click', (event) => {
+    event.preventDefault();
+    const main = document.querySelector('main');
+    const parksList = document.querySelectorAll('.park-display');
+    main.innerHTML = '';
+  
+
+  const parksArray = Array.from(parksList);
+
+  parksArray.sort((parkA, parkB) => {
+    const parkAName = parkA.querySelector('h2').innerText;
+    const parkBName = parkB.querySelector('h2').innerText;
+    if (parkAName < parkBName) {
+      return -1;
+    } else if (parkAName > parkBName) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+});
+
+// // Function for sorting by name
+// const sortByName = (parkA, parkB) => {
+//   const parkAName = parkA.querySelector("h2").innerText;
+//   const parkBName = parkB.querySelector("h2").innerText;
+//   if (parkAName < parkBName) {
+//     return -1;
+//   } else if (parkAName > parkBName) {
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+// };
+
+// // Function for handling the `nameSorter` click
+// const nameSorterClickHandler = (event) => {
+//   event.preventDefault();
+//   const main = document.querySelector("main");
+//   const parksList = main.querySelectorAll(".park-display");
+//   main.innerHTML = "";
+//   const parksArray = Array.from(parksList);
+//   parksArray.sort(sortByName);
+//   parksArray.forEach((park) => {
+//     main.appendChild(park);
+//   });
+// };
+
+// const nameSorter = document.querySelector("#name-sorter");
+// nameSorter.addEventListener("click", nameSorterClickHandler);
+
+/*------------------ updating the dom ------------*/
 const descriptions = document.querySelectorAll('.description-display')
 
 for (let desc of descriptions.values()) {
@@ -22,7 +140,7 @@ for (let rating of ratings) {
 
 
 const parks = document.querySelectorAll('.park-display');
-const numberParks =parks.length;
+const numberParks = parks.length;
 const newElement = document.createElement('div');
 
 newElement.innerText = `${numberParks} exciting parks to visit`;
@@ -32,9 +150,9 @@ const header = document.querySelector('header');
 header.appendChild(newElement)
 
 
-const main = document.querySelector('main');
-const park = main.querySelector('.park-display');
-main.removeChild(park);
+// const main = document.querySelector('main');
+// const park = main.querySelector('.park-display');
+// main.removeChild(park);
 
 /*---------------selecting Elements on DOM-----------*/
 // console.log(document);
